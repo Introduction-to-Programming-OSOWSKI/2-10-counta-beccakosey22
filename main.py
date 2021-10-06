d07bbda398cf5202a363f611d061cfcb7425dc0d
beccakosey22 <820512@mystma.org>
2021-10-06 09:12:49 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#WRITE YOUR CODE IN THIS FILE
+#Create a function called countA() that returns the number of "a"s in a given word w
+def countA(w):
+    
+    total = 0
+    
+    for i in range(0, len(w)):
+        if w[i] == "a":
+            total = total + 1
+
+    return total
+
+print(countA("cat"))
